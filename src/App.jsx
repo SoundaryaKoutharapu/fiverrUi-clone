cfed233af1fa4cf7469d5bcb96d6d0a8e5828709
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-07-27 20:14:30 +0530

    added sticky navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ function App() {
     <>
      <div>
      <Navbar/>
+     <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque mollitia quasi tempore adipisci accusantium laudantium molestias assumenda temporibus ipsum veniam harum minima, nulla, repellat reiciendis earum, quisquam itaque incidunt ipsa.</h1>
+     <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia distinctio ratione hic cum ullam placeat eos laboriosam voluptas reprehenderit ipsa culpa dolore accusamus facere praesentium tempore, necessitatibus exercitationem, quasi repudiandae?</h1>
+     <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis dolor dolore praesentium perspiciatis. Id a ipsa officiis, dolor placeat itaque corrupti est explicabo aliquam aliquid autem! Optio quaerat totam excepturi.</h1>
+     <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorum assumenda quasi quo accusamus rerum eos quod omnis, dolorem enim nostrum illum quisquam? Dolores, tempore deserunt exercitationem eum iusto voluptas!</h1>
+     <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, aspernatur provident quod assumenda repellat doloremque! Eaque pariatur error tenetur, at ea amet aut, eum, nihil iusto vero harum distinctio ratione!</h1>
+     <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti aut necessitatibus sunt nam deleniti quas ipsam, qui libero veniam quis quidem tenetur mollitia ipsum, eveniet, dolor facere optio fuga. Eum.</h1>
+     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maxime ad tenetur quidem facere rerum optio cum, autem dignissimos rem et repudiandae corrupti perferendis non voluptas eveniet iure dolores harum?</h1>
      </div>
     </>
   )
--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,24 +1,52 @@
+import { useState } from 'react'
 import '../navbar/Navbar.scss'
+import { Fragment } from 'react'
+import { useEffect } from 'react'
 
 const Navbar = () => {
+
+    const [active, setActive] = useState(false)
+
+    const isActive = () => {
+        window.scrollY > 0 ? setActive(true) : setActive(false)
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', isActive)
+
+        return () => {
+            window.removeEventListener('scroll', isActive)
+        }
+    }, [])
+
     return (
-        <div className='navbar'>
+        <div className={active ? 'navbar active' : 'navbar'}>
             <div className='container'>
-            <div className='logo'>
-               <Link to='/'>
-                <span className='text'>fiverr</span>
-                <span className='dot'>.</span>
-                </Link>
+                <div className='logo'>
+                    {/* <Link to='/'> */}
+                    <span className='text'>fiverr</span>
+                    <span className='dot'>.</span>
+                    {/* </Link> */}
+                </div>
+                <div className='links'>
+                    <span>Fiverr Business</span>
+                    <span>Explore</span>
+                    <span>English</span>
+                    <span>Become a Seller</span>
+                    <span>Sign In</span>
+                    <button>Join</button>
+                </div>
             </div>
-            <div className='links'>
-                <span>Fiverr Business</span>
-                <span>Explore</span>
-                <span>English</span>
-                <span>Become a Seller</span>
-                <span>Sign In</span>
-              <button>Join</button>
-            </div>
-            </div>
+            {active && (
+                <>
+                    <hr />
+                    <div className='menu'>
+                        <span>Test</span>
+                        <span>Test2</span>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
